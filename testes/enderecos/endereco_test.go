abfd2888e835146d4639f628ef50d19ee9b02f24
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-08-27 11:16:53 -0300

    Ferramentas de Testes em go

---
+++ testes/enderecos/endereco_test.go
@@ -0,0 +1,103 @@
+package enderecos
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Scenario struct {
+	givenAddress string
+	expectedAddress string
+}
+
+func AssertHelper(result, expected interface{}, t *testing.T) {
+	t.Helper()
+
+	if result != expected {
+		t.Errorf("Result: %s, Expected: %s", result, expected)
+	}
+
+}
+
+
+
+func TestTipoEnderecoWithScenarios(t *testing.T){
+	scenarios := []Scenario{
+		{givenAddress: "Rua ABC", expectedAddress: "Rua"},
+		{givenAddress: "Avenida Brigadeiro Faria Lima, 2447", expectedAddress:"Avenida"},
+		{givenAddress: "Praça da Sé", expectedAddress: "Tipo Inválido"},
+		{givenAddress: "Jardim Botânico de Jundiaí", expectedAddress: "Tipo Inválido"},
+		{givenAddress: "Rodovia dos Imigrantes, Km. 23", expectedAddress: "Rodovia"},
+		{givenAddress: "Estrada do M'boi Mirim, km 12", expectedAddress: "Estrada" },
+		{givenAddress: "", expectedAddress: "Tipo Inválido"},
+	}
+
+
+	for _, scenario := range scenarios {
+		result := TipoDeEndereco(scenario.givenAddress)
+		AssertHelper(result, scenario.expectedAddress, t)
+	}
+}
+
+
+func TestTipoEndereco(t *testing.T) {
+
+	t.Run("Avenida", func(t *testing.T) {
+		endereco := "Avenida Paulista, 1890"
+		result := TipoDeEndereco(endereco)
+		expected := "Avenida"
+
+		AssertHelper(result, expected, t)
+	})
+
+	
+
+	t.Run("Rua", func(t *testing.T) {
+		endereco := "Rua Augusta, 2356"
+
+		result := TipoDeEndereco(endereco)
+		expected := "Rua"
+
+		AssertHelper(result, expected, t)
+	})
+
+
+	t.Run("RUA CAPITALIZED", func(t *testing.T) {
+		endereco := "RUA DOS BOBOS"
+		result := TipoDeEndereco(endereco)
+
+		expected := "Rua"
+
+		AssertHelper(result, expected, t)
+	})
+
+
+	t.Run("Rodovia Anhaguera", func(t *testing.T) {
+		endereco := "Rodovia Anhanguera, Km 60"
+
+		result := TipoDeEndereco(endereco)
+		expected := "Rodovia"
+
+		AssertHelper(result, expected, t)
+	})
+
+}
+
+func TestContains(t *testing.T) {
+	quote := "Quick brown fox jumps over the lazy dog"
+	word := "fox"
+
+	result := Contains(quote, word)
+	expected := true
+
+	AssertHelper(result, expected, t)
+}
+
+func ExampleContains() {
+	endereco := "Avenida Cruzeiro do Sul, 3456"
+	result := TipoDeEndereco(endereco)
+
+	fmt.Println(result)
+
+	// Output: Avenida
+}
